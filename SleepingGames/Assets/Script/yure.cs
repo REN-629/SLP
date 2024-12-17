623d7e0e6388a35369d5365e80fe47f78c15e9b3
Hyde_306 <xma504986@gmail.com>
2024-12-17 10:12:05 +0900

    ゆれいじった

--- SleepingGames/Assets/Script/yure.cs
+++ SleepingGames/Assets/Script/yure.cs
@@ -5,9 +5,16 @@ public class yure : MonoBehaviour
     public yurecamera cameraShake;
     public float shakeDuration = 0.5f;
     public float shakeMagnitude = 0.01f;
+    public string ignoreTag = "NoShake"; // 揺らさないオブジェクトのタグ
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        // 衝突したオブジェクトが特定のタグを持っている場合、揺れを無効にする
+        if (collision.gameObject.CompareTag(ignoreTag))
+        {
+            return;
+        }
+
         StartCoroutine(cameraShake.Shake(shakeDuration, shakeMagnitude));
     }
 }
