b3ca7552cd380b32b88aa1ad7205f1737ae3a0c0
REN <reny58817@gmail.com>
2025-01-09 12:14:21 +0900

    エンドロール作り

--- SleepingGames/Assets/Script/fadetimer.cs
+++ SleepingGames/Assets/Script/fadetimer.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
 using System.Collections;
 
-public class fade : MonoBehaviour
+public class fadetimer : MonoBehaviour
 {
     public float fadeDuration = 2f; // フェードインの時間
+    public float delayDuration = 1f; // フェードイン開始前の待機時間
     private CanvasGroup canvasGroup;
 
     void Start()
     {
         canvasGroup = GetComponent<CanvasGroup>();
         canvasGroup.alpha = 0f;
+        StartCoroutine(DelayedFadeIn());
+    }
+
+    IEnumerator DelayedFadeIn()
+    {
+        yield return new WaitForSeconds(delayDuration); // 指定した秒数待機
         StartCoroutine(FadeIn());
     }
 
