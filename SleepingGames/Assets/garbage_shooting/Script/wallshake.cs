6cb160681be7c20597e6065fdd81fe98b75d793d
REN <reny58817@gmail.com>
2025-01-15 10:27:20 +0900

    Revert "no message"
    This reverts commit 74028f5953cd42062c3f0a94cc6361c28d5d8e9c.

--- SleepingGames/Assets/garbage_shooting/Script/wallshake.cs
+++ SleepingGames/Assets/garbage_shooting/Script/wallshake.cs
@@ -8,31 +8,7 @@ public class wallshake : MonoBehaviour
     {
         if (other.gameObject.CompareTag("tukue"))
         {
-            // オブジェクトを揺らすエフェクトを追加
-            StartCoroutine(Shake());
+            
         }
-    }
-
-    private IEnumerator Shake()
-    {
-        Vector3 originalPosition = transform.position;
-        float duration = 0.5f;
-        float magnitude = 0.1f;
-
-        float elapsed = 0.0f;
-
-        while (elapsed < duration)
-        {
-            float x = Random.Range(-1f, 1f) * magnitude;
-            float y = Random.Range(-1f, 1f) * magnitude;
-
-            transform.position = new Vector3(originalPosition.x + x, originalPosition.y + y, originalPosition.z);
-
-            elapsed += Time.deltaTime;
-
-            yield return null;
-        }
-
-        transform.position = originalPosition;
     }
 }
