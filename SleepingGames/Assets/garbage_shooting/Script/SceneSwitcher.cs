0653cb065a7e6aa1319a3474a7a225f0edd193f7
nakanishi <h1202419017@hiratagakuen.onmicrosoft.com>
2025-01-21 09:56:59 +0900

    no message

--- SleepingGames/Assets/garbage_shooting/Script/SceneSwitcher.cs
+++ SleepingGames/Assets/garbage_shooting/Script/SceneSwitcher.cs
@@ -7,20 +7,20 @@ public class SceneSwitcher : MonoBehaviour
     public string SceneToLoad;  // 切り替えたいシーンの名前（Unityで名前を設定する）
     public float Delay = 0f;    // シーンを切り替えるまでの遅延時間（Unityで時間と設定する）
 
-    //OnCollisionEnter2D関数
+    //オンコリュージョンエンター2D関数
     //特定のオブジェクトに接触するとシーン移行のプログラムを実行する
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("gomi"))//gomiのTagが付いた物に当たると以下のプログラムを実行する
         {
             Debug.Log("特定のオブジェクトに接触しました。シーンを切り替えます。");
-            StartCoroutine(ChangeSceneAfterDelay());
+            StartCoroutine(ChangeScene());
         }
     }
 
-    //ChangeSceneAfterDelay関数
+    //チェンジシーン関数
     //指定した秒数が経過するとシーンが切り替わるプログラム
-    private IEnumerator ChangeSceneAfterDelay()
+    private IEnumerator ChangeScene()
     {
         Debug.Log("シーン切り替えのための遅延を開始します。");
         yield return new WaitForSeconds(Delay); // 遅延時間を待機
